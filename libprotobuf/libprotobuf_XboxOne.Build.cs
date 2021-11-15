d82400a6ca0780d8dd53bf5c93dc70b039bee4af
thejinchao <thejinchao@gmail.com>
2021-11-15 13:10:59 +0800

    add XBoxOne compiled library

---
+++ libprotobuf/libprotobuf_XboxOne.Build.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+using UnrealBuildTool;
+using System.IO;
+
+public class libprotobuf_XboxOne : libprotobuf
+{
+	public libprotobuf_XboxOne(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		string protobufPath = ModuleDirectory;
+		
+		PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "xboxone", "protobuf.lib"));
+		PublicSystemIncludePaths.Add(Path.Combine(protobufPath, "include"));
+	}
+}
